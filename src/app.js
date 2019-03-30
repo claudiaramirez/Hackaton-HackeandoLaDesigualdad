2dd85006238544dfb4971c53c9c46ea1c21991cc
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-03-30 14:48:31 -0600

    menu2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
   }); 
 
